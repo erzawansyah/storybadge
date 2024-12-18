ab8d157a9f3b53516b95e6f880ddb03e2013893c
Muhamad Erza Wansyah <erzawansyah@gmail.com>
2024-12-19 04:18:22 +0700

    Little change for variable naming

--- src/lib/supabase/supa-client.ts
+++ src/lib/supabase/supa-client.ts
@@ -3,6 +3,6 @@ import { Database } from "./database.types";
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient<Database>(
-  `https://${process.env.NEXT_PUBLIC_SUPABASE_CLIENT_ID!}.supabase.co`,
+  `https://${process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID!}.supabase.co`,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
